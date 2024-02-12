5b7420bce339a08b23dd92e7957b0c60dfba4282
DIMIBAGA <dianabarrantesga1510@gmail.com>
2024-02-12 12:38:35 -0500

    commit

--- Main.java
+++ Main.java
@@ -1,15 +1,6 @@
-public class Main{
+public class Main {
     public static void main(String[] args) {
-        int num = 5; // Puedes cambiar el número aquí para calcular su factorial
-        int factorial = f(num);
-        System.out.println("El factorial de " + num + " es: " + factorial);
-    }
-
-    public static int f(int n) {
-        if (n == 0) {
-            return 1;
-        } else {
-            return n * f(n - 1);
-        }
+        System.out.println("Este es un compilador empírico 🤑.");
     }
 }
+                
