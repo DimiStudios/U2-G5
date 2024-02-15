6d0de8312bbe89b9f3bfb65342a002a8631ef94f
DIMIBAGA <dianabarrantesga1510@gmail.com>
2024-02-14 22:21:18 -0500

    COMMIT

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Este es un compilador empírico 🤑.");
+        System.out.println("Dal");
     }
 }
                 
